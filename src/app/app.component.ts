39b68e094fd4e3aba9ba1814ee90d3b48a297749
Omar Hosny <honsyomar022@gmail.com>
2022-10-07 17:20:11 +0200

    Data Binding  Section Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // encapsulation: ViewEncapsulation.None, // make the css properties applied glopaly
 })
 export class AppComponent {
-  title = 'my-second-app-angular';
+  // serverElements = [];
+  // onServerAdded(serverDate: { serverName: string; serverContent: string }) {
+  //   this.serverElements.push({
+  //     type: 'server',
+  //     name: serverDate.serverName,
+  //     content: serverDate.serverContent,
+  //   });
+  // }
+  // onBlueprintAdded(blueprintData: {
+  //   serverName: string;
+  //   serverContent: string;
+  // }) {
+  //   this.serverElements.push({
+  //     type: 'blueprint',
+  //     name: blueprintData.serverContent,
+  //     content: blueprintData.serverContent,
+  //   });
+  // }
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 == 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 }
